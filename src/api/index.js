77f986067c6abc42abb6768cfcdc3b29d3bedb8c
Abhishek Kumar <72213318+AK-ABHISHEK-KUMAR@users.noreply.github.com>
2022-12-13 23:01:45 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const URL = process.env.WEB_URL || "http://localhost:3001"; 
+
 export default axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: URL,
 });
